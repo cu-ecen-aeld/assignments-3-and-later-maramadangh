81c66a226184be1864378cfff94fb1b327635f81
root <root@DE01NB136.brusa.biz>
2025-07-18 08:48:59 +0200

    fixing typo

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -101,7 +101,7 @@ echo -e '#!/bin/sh\nmount -t proc none /proc\nmount -t sysfs none /sys\nexec /bi
 chmod 777 ${OUTDIR}/rootfs/init
 cp ${GIT_ROOT}/finder-app/{writer.c,writer.sh,start-qemu-app.sh,start-qemu-terminal.sh,Makefile,finder-test.sh,finder.sh,autorun-qemu.sh} ${OUTDIR}/rootfs/home/
 echo result of coping is $?
-chmod 777 findr-test.sh
+chmod 777 finder-test.sh
 mkdir -p ${OUTDIR}/rootfs/home/conf
 cp ${GIT_ROOT}/finder-app/conf/* ${OUTDIR}/rootfs/home/conf/
 cd ${OUTDIR}/rootfs/home
